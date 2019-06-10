f44147d067a34787a81358202806156733cb18dd
Jared De Blander <jared@deblander.org>
2019-06-10 03:52:40 -0400

    Updates to samples

--- Kotlin101/types-min-max.kt
+++ Kotlin101/types-min-max.kt
@@ -1,8 +1,9 @@
 
 fun main() {
-    range(Int)
+    range("Int",Int.MIN_VALUE, Int.MAX_VALUE)
+    
 }
 
-fun range(x: Any) {
-    println(x.MAX_VALUE)
+fun range(name: String, lower: Any, upper: Any) {
+    println("$name: Lower Bound: $lower, Upper Bound: $upper")
 }
